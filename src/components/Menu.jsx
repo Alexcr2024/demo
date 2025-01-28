7719f50268fc4f9983a7c1c09ecb8d3ebb94bf7c
Jarliev Perez <55269430+Thotsuya@users.noreply.github.com>
2025-01-27 23:37:07 -0600

    New Presentation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,58 +31,71 @@ export default function Menu() {
     <nav className="navbar">
       <ul className="navbar-list">
         <li>
-        <li>
-        <ul className="languagepicker">
-          {selectedLanguage === 'es' ? (
-            <>
-              <li>
-                <Link href="es" onClick={() => handleLanguageChange('es')}>
-                  <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/spain.png" alt="spain" />
-                  Español
-                </Link>
-              </li>
-              <li>
-                <Link href="en" onClick={() => handleLanguageChange('en')}>
-                  <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/usa.png" alt="usa" />
-                  English
-                </Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link href="en" onClick={() => handleLanguageChange('en')}>
-                  <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/usa.png" alt="usa" />
-                  English
-                </Link>
-              </li>
-              <li>
-                <Link href="es" onClick={() => handleLanguageChange('es')}>
-                  <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/spain.png" alt="spain" />
-                  Español
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </li>
+          <li>
+            <ul className="languagepicker">
+              {selectedLanguage === 'es' ? (
+                  <>
+                    <li>
+                      <Link href="es" onClick={() => handleLanguageChange('es')}>
+                        <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/spain.png"
+                                       alt="spain"/>
+                        Español
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="en" onClick={() => handleLanguageChange('en')}>
+                        <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/usa.png" alt="usa"/>
+                        English
+                      </Link>
+                    </li>
+                  </>
+              ) : (
+                  <>
+                    <li>
+                      <Link href="en" onClick={() => handleLanguageChange('en')}>
+                        <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/usa.png" alt="usa"/>
+                        English
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="es" onClick={() => handleLanguageChange('es')}>
+                        <LazyLoadImage width="20" height="20" src="https://img.icons8.com/color/48/spain.png"
+                                       alt="spain"/>
+                        Español
+                      </Link>
+                    </li>
+                  </>
+              )}
+            </ul>
+          </li>
+        </li>
+        <li className="navbar-item">
+          <Link href={'https://cotizacion.vascotechnology.com/login'}>
+            Cotizar
+          </Link>
+        </li>
+
+        <li className="navbar-item">
+          <Link href={'https://crm.vascotechnology.com/login'}>
+            CRM
+          </Link>
         </li>
 
         <li className="navbar-item" onClick={handleLogout}>
           <span className="left">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              height={25}
-              width={25}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                height={25}
+                width={25}
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
               />
             </svg>
           </span>
